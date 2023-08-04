2996bff021ed16ae7d2911f808e743119f84eb67
emregozdamla <madenciemre01@gmail.com>
2023-08-04 19:14:03 +0300

    yilan

---
+++ Yilan_Oyunu/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Yilan_Oyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 200;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(275, 255);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 59);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Oyuna Başla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(482, 255);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 59);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Çıkış";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(223, 246);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(95, 50);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Yeniden Oyna";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(470, 246);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(96, 50);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Çıkış";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(645, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "label2";
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(223, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(343, 73);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "label3";
+            this.label3.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(600, 600);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 669);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Yilan_Oyunu/Form1.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Yilan_Oyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        List<Label> snakeParts = new List<Label>();
+        public static Label food;
+        public static Label label1;
+        public static Label label4;
+        public string direction = "";
+        public int snakeHeight = 1;
+        public int point = 0;
+        public int unitSize = 20;
+        public bool gameOver=false;
+        public void AddPauseText()
+        {
+            label4 = new Label();
+            label4.Font = new Font("Verdana", 22);
+            label4.BackColor = Color.Empty;
+            label4.Visible = false;
+            label4.ForeColor = Color.Blue;
+            label4.Text = "Oyun Donduruldu";
+            label4.Location = new Point(170, 270);
+            label4.Size = new Size(300, 200);
+            pictureBox1.Controls.Add(label4);
+
+            
+        }
+        public void RemovePauseText()
+        {
+            pictureBox1.Controls.Remove(label4);
+        }
+        public void CheckCollision()
+        {
+            int i = 3;
+            while(i<snakeParts.Count)
+            { 
+                if (snakeParts[0].Location.X == snakeParts[i].Location.X
+                    && snakeParts[0].Location.Y == snakeParts[i].Location.Y )
+                {
+                    gameOver = true;
+                }
+                i++;
+            }
+        }
+        public void EatFoodControl()
+        {
+            int i = 0;
+            while(i<snakeParts.Count)
+            {
+                if (snakeParts[i].Location.X<food.Location.X+unitSize
+                    && snakeParts[i].Location.X > food.Location.X - unitSize
+                    && snakeParts[i].Location.Y > food.Location.Y - unitSize
+                    && snakeParts[i].Location.Y < food.Location.Y + unitSize)
+                {
+                    pictureBox1.Controls.Remove(food);
+                    snakeHeight++;
+                    point++;
+                    PutFood();
+                    if(timer1.Interval>=20)
+                    {
+                        timer1.Interval -= 10;
+                    }
+                }
+                i++;
+            }
+
+        }
+        public void FormSnakeParts()
+        {
+            
+            int i = snakeParts.Count;
+            while(i<=snakeHeight)
+            {
+                Label part = new Label();
+                part.Size = new Size(unitSize, unitSize);
+                part.BackColor = Color.Black;
+                snakeParts.Add(part);
+                i++;
+            }
+            i = snakeParts.Count - 1;
+            while(i>0)
+            {
+                snakeParts[i].Location = snakeParts[i - 1].Location;
+                i--;
+            }
+            i = 1;
+            while (i <= snakeHeight)
+            {
+
+                pictureBox1.Controls.Add(snakeParts[i]);
+                i++;
+            }
+
+        }
+        public void PutFood()
+        {
+            food = new Label();
+            food.Size = new Size(unitSize, unitSize);
+            food.BackColor = Color.Red;
+            Random random = new Random();
+            int foodX = random.Next(0, 29) * unitSize;
+            int foodY= random.Next(0, 29) * unitSize;
+            int i = 0;
+            while(i<snakeParts.Count)
+            {
+                if (snakeParts[i].Location.X==foodX && snakeParts[i].Location.Y==foodY)
+                {
+                    PutFood();
+                }
+                i++;
+            }
+            food.Location = new Point(foodX, foodY);
+            pictureBox1.Controls.Add(food);
+        }
+        public void FormHead()
+        {
+            label1 = new Label();
+            label1.BackColor = Color.Black;
+            label1.Location = new Point(280, 280);
+            label1.Size = new Size(unitSize, unitSize);
+            pictureBox1.Controls.Add(label1);
+            snakeParts.Add(label1);
+        }
+        public void PassWalls()
+        {
+            if (label1.Location.Y<0) label1.Location = new Point(label1.Location.X,600-unitSize);
+            if (label1.Location.Y > 600) label1.Location = new Point(label1.Location.X, 0);
+            if (label1.Location.X < 0) label1.Location = new Point(600-unitSize, label1.Location.Y);
+            if (label1.Location.X > 600) label1.Location = new Point(0, label1.Location.Y);
+        }
+        public void DetermineHeadDirection(KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.W && direction != "Down")
+            {
+                direction = "Up";
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+            }
+            if (e.KeyCode == Keys.S && direction != "Up")
+            {
+                direction = "Down";
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+            }
+            if (e.KeyCode == Keys.A && direction != "Right")
+            {
+                direction = "Left";
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+            }
+            if (e.KeyCode == Keys.D && direction != "Left")
+            {
+                direction = "Right";
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+            }
+            if (e.KeyCode == Keys.W )
+            {
+                
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+            }
+            if (e.KeyCode == Keys.S )
+            {
+                
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+            }
+            if (e.KeyCode == Keys.A )
+            {
+                
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+            }
+            if (e.KeyCode == Keys.D )
+            {
+                
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+            }
+        }
+        public void MoveHead()
+        {
+            //if (direction == "Up") label1.Top -= unitSize;
+            //if (direction == "Down") label1.Top += unitSize;
+            //if (direction == "Right") label1.Left += unitSize;
+            //if (direction == "Left") label1.Left -= unitSize;
+            if (direction == "Up") label1.Location = new Point(label1.Location.X, label1.Location.Y - unitSize);
+            if (direction == "Down") label1.Location = new Point(label1.Location.X, label1.Location.Y + unitSize);
+            if (direction == "Right") label1.Location = new Point(label1.Location.X + unitSize, label1.Location.Y);
+            if (direction == "Left") label1.Location = new Point(label1.Location.X - unitSize, label1.Location.Y);
+        }
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            MoveHead();
+            PassWalls();
+            EatFoodControl();
+            FormSnakeParts();
+            CheckCollision();
+            //AddPauseText();
+            label2.Text = "Puanınız: " + point;
+            if(gameOver==true)
+            {
+                timer1.Stop();
+                timer1.Interval = 200;
+                
+                snakeHeight = 1;
+                snakeParts.Clear();
+                pictureBox1.Controls.Clear();
+                pictureBox1.Visible = false;
+                label2.Visible = false;
+                button3.Visible = true;
+                button3.Enabled = true;
+                button4.Visible = true;
+                button4.Enabled = true;
+                label3.Visible = true;
+                label3.Text = "Puanınız: " + point;
+                label3.TextAlign = ContentAlignment.MiddleCenter;
+                
+                direction = "";
+                
+
+            }
+
+        }
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            DetermineHeadDirection(e);
+            if (e.KeyCode == Keys.Space)
+            {
+                if (timer1.Enabled && gameOver == false)
+                {
+                    timer1.Stop();
+                    label4.Visible = true;
+                    e.Handled = true;
+                    e.SuppressKeyPress = true;
+
+                }
+                else if (!timer1.Enabled && gameOver == false)
+                {
+                    timer1.Start();
+                    label4.Visible = false;
+                    e.Handled = true;
+                    e.SuppressKeyPress = true;
+                }
+            }
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Visible = true;
+            label2.Visible = true;
+
+            button1.Visible = false;
+            button2.Visible = false;
+            button1.Enabled = false;
+            button2.Enabled = false;
+            direction = "";
+            FormHead();
+            PutFood();
+            AddPauseText();
+            timer1.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.ExitThread();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Application.ExitThread();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Visible = true;
+            label2.Visible = true;
+            gameOver = false;
+            label3.Visible = false;
+            button3.Visible = false;
+            button3.Enabled = false;
+            button4.Visible = false;
+            button4.Enabled = false;
+            point = 0;
+            direction = "";
+            FormHead();
+            PutFood();
+            AddPauseText();
+            timer1.Start();
+        }
+    }
+}
